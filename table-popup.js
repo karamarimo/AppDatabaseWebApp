aac638af30282ca60d525704f449ce383272a7a8
karamarimo <admin@pc-b094.local>
2016-11-22 16:36:57 +0900

    update table-popup.js
    now it does not do anything while the popup is fading out or in to
    avoid unintended behavior

--- table-popup.js
+++ table-popup.js
@@ -1,14 +1,18 @@
 $(function() {
+	var fading = false;
 	$('.table-popup tr').click(function (event) {
 		var ref = $(this).attr('data-href');
-		if (ref) {
+		if (ref && !fading) {
 			// window.location.href = ref;
 			// var popup = $('#popup-content').get(0);
 			// if (popup) {
 			// 	popup.load(ref, function () {});
 			// }
+			fading = true;
 			$('#popup-content').load(ref, function() {
-				$('#popup').fadeIn(300).scrollTop(0).css('display', 'flex');
+				$('#popup').fadeIn(300, function() {
+					fading = false;
+				}).scrollTop(0).css('display', 'flex');
 				$('body').css('overflow', 'hidden');
 			});
 		}
@@ -16,8 +20,11 @@ $(function() {
 
 	// close popup
 	$('#popup').click(function(event) {
-		if (event.target == this) {
-			$(this).fadeOut(300);
+		if (event.target == this && !fading) {
+			fading = true;
+			$(this).fadeOut(300, function(){
+				fading = false;
+			});
 			$('body').css('overflow', 'initial');
 		}
 	});
